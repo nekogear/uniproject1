4ad8e74d03e3b9ac823406f40302a1cca770a367
nekogear <146372211+nekogear@users.noreply.github.com>
2025-07-13 02:29:29 +0700

    Add files via upload

---
+++ js/forms.js
@@ -0,0 +1,7 @@
+function myFunction() {
+     var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");}
+
+function interactiveFunction(){
+    let feedback = prompt("i need some feedback so please leave a comment, i will definately check :))))")
+    }
